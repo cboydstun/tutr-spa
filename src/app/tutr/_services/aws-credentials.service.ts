7bad175a7609d1f8e5aef0d3b61511fc74dbaef7
Platon Korzh <platonkorzh@Platons-MacBook-Pro.local>
2017-10-08 17:17:34 +0300

    AwsCredentialsService

--- src/app/tutr/_services/aws-credentials.service.ts
+++ src/app/tutr/_services/aws-credentials.service.ts
@@ -4,10 +4,16 @@ import { Injectable } from '@angular/core';
 
 import { CognitoService } from './cognito.service';
 
+import { environment } from "../../../environments/environment";
+
 @Injectable()
 export class AwsCredentialsService {
 
-	constructor(private cognitoService: CognitoService) { }
+	constructor(private cognitoService: CognitoService) { 
+		AWS.config.update({
+			region: environment.region
+		});
+	}
 
 	public init(idTokenJwt: string) {
 		const creds = this.cognitoService.buildCognitoCreds(idTokenJwt);
