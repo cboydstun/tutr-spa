21178da5dd0de0946fd0d3ae2baba43fa503a4de
Platon Korzh <platon.korzh@teaminternational.com>
2017-10-12 16:41:47 +0300

    remove tenant routing module

--- src/app/tenant/tenant.module.ts
+++ src/app/tenant/tenant.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { TenantRoutingModule } from './tenant-routing.module';
-
 @NgModule({
 	imports: [
-		CommonModule,
-		TenantRoutingModule
+		CommonModule
 	],
 	declarations: []
 })
